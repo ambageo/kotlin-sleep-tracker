f9ab0d0a505fc4a3c9a81180e0e36a136773da10
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-04-23 12:13:07 +0100

    Created the RoomDatabase

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -16,16 +16,66 @@
 
 package com.example.android.trackmysleepquality.database
 
-// TODO (01) Create an abstract class that extends RoomDatabase.
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
-// TODO (02) Declare an abstract value of type SleepDatabaseDao.
+// TODO (01) Create an abstract class that extends RoomDatabase. //DONE
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase(){
 
-// TODO (03) Declare a companion object.
+    // TODO (02) Declare an abstract value of type SleepDatabaseDao. //DONE
+    abstract val sleepDatabaseDao: SleepDatabaseDao
 
-// TODO (04) Declare a @Volatile INSTANCE variable.
+    // TODO (03) Declare a companion object. //DONE
+    companion object {
 
-// TODO (05) Define a getInstance() method with a synchronized block.
+        // TODO (04) Declare a @Volatile INSTANCE variable. //DONE
+        /*
+        * Volatile prevents the variable from being cached; any writing or reading will be done from
+        * the main memory. So, the value is always up to date and the same across all threads.
+        * If a thread changes the value, it is instantly visible to all other threads.
+        */
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
 
-// TODO (06) Inside the synchronized block:
-// Check whether the database already exists,
-// and if it does not, use Room.databaseBuilder to create it.
+        // TODO (05) Define a getInstance() method with a synchronized block. //DONE
+        fun getInstance(context: Context):SleepDatabase{
+            /*
+            * With synchronized we make sure that only one thread at a time will have access to the
+            * getInstance() , which makes sure that the database will get initialized only once.
+            */
+            synchronized(this){
+                var instance = INSTANCE
+
+                // TODO (06) Inside the synchronized block: //DONE
+                // Check whether the database already exists,
+                // and if it does not, use Room.databaseBuilder to create it.
+                if(instance == null){
+                    instance = Room.databaseBuilder(context.applicationContext,
+                            SleepDatabase::class.java,
+                            "sleep_history_database")
+                            // This handles what to do in case of updating the app (either by also
+                            // updating/changing the schema or not. In our case we just destroy the
+                            // old tables and create them all over (therefore,  data is lost)
+                            // link in Kotlin Nanodegree folder explains all different cases
+                            .fallbackToDestructiveMigration()
+                            .build()
+                }
+                return instance
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
