217ce8dc6b214c23fa10167c8807ea7afa60516d
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-04-25 10:41:50 +0100

    Added the ViewModel using ViewModelFactory

--- app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityFragment.kt
@@ -22,8 +22,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepQualityBinding
+import com.example.android.trackmysleepquality.sleeptracker.SleepTrackerViewModel
+import com.example.android.trackmysleepquality.sleeptracker.SleepTrackerViewModelFactory
 
 /**
  * Fragment that displays a list of clickable icons,
@@ -46,6 +50,12 @@ class SleepQualityFragment : Fragment() {
                 inflater, R.layout.fragment_sleep_quality, container, false)
 
         val application = requireNotNull(this.activity).application
+
+        binding.lifecycleOwner = this
+
+
+
+
 
         return binding.root
     }
